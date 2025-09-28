a02f9ff388590bb1dfa47de951f386ece022b8ad
OIAST <david89813@gmail.com>
2025-09-28 15:56:28 +0800

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_session_state():
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
-    if "login_error" not in st.session_state:
-        st.session_state.login_error = False
-    if "username" not in st.session_state:
-        st.session_state.username = ""
